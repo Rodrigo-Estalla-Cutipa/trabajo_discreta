1ea35586639fad0f957880143521b8507ef156ca
Rodrigo-Estalla-Cutipa <restallac@unjbg.edu.pe>
2025-12-09 15:36:13 -0500

    Programa que puede multiplicar vector a con b

--- main.cpp
+++ main.cpp
@@ -1,2 +1,37 @@
 #include <iostream>
-///texto de prueba Rodrigo
+#include "vectores.h"
+#include "multiplicar.h"
+using namespace std;
+
+int main(){
+    vectores a, b;
+    int num;
+    system("cls");
+    cout<<"MULTIPLICACION VECTORIAL\n\n";
+    cout <<"Ingrese los valores:\n\n";
+    cout <<"Vector a:\n";
+    cout <<"\ti: "; cin >> a.i;
+    cout <<"\tj: "; cin >> a.j;
+    cout <<"\tk: "; cin >> a.k;
+
+    cout <<"Vector b:\n";
+    cout <<"\ti: "; cin >> b.i;
+    cout <<"\tj: "; cin >> b.j;
+    cout <<"\tk: "; cin >> b.k;
+
+    cout << "\nEscoger multiplicacion:\n";
+    cout << "1. (a x b)\n";
+    cout << "2. (b x a)\n";
+    cout << "Opcion nro: "; cin >> num;
+
+    if(num==1){
+        cout << "\nProducto Vectorial: ";
+        valorR(a,b);
+    } else if (num==2){
+        cout << "\nProducto Vectorial: ";
+        valorR(b,a);
+    } else {
+        cout << "Opcion Inválida";
+    }
+    return 0;
+}
